8b40bb6c1df6d21d5464df96cb932c05d455194a
FiveTW87 <thanwalokwattanakul@gmail.com>
2024-01-08 12:48:45 +0700

    feat : get /assignments

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// Start coding here
+// Start coding
+import express from "express";
+
+import { assignments } from "./data/assignments";
+
+let assignmentsMogDatabase = assignments;
+
+const app = express();
+const port = 3000;
+
+app.get("/assignments", function (req, res) {
+  return res.json({ data: assignmentsMogDatabase });
+});
+
+app.listen(port, () => {
+  console.log(`this sever is running at ${port}`);
+});
