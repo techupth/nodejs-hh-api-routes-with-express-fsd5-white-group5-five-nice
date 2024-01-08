4e430a89b540ebf3e6ee8c8948d3347a4b91b718
FiveTW87 <thanwalokwattanakul@gmail.com>
2024-01-08 13:45:48 +0700

    feat : post assignments

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ let assignmentsMogDatabase = assignments;
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/assignments", function (req, res) {
   const limit = req.query.limit;
   if (limit > 10) {
@@ -16,7 +19,32 @@ app.get("/assignments", function (req, res) {
     });
   }
   const assignmentsWithLimit = assignmentsMogDatabase.slice(0, limit);
-  return res.json({ data: assignmentsWithLimit });
+  return res.json({
+    message: "Complete Fetching assignments",
+    data: assignmentsWithLimit,
+  });
+});
+
+app.post("/assignments", function (req, res) {
+  assignmentsMogDatabase.push({
+    id: assignmentsMogDatabase[assignmentsMogDatabase.length - 1].id + 1,
+    ...req.body,
+  });
+  return res.json({
+    message: "New assignment has been created successfully",
+    data: assignmentsMogDatabase[assignmentsMogDatabase.length - 1],
+  });
+});
+
+app.get("/assignments/:assignmentsId", function (req, res) {
+  let assignmentsFromClient = Number(req.params.assignmentsId);
+  let assignmentsData = assignmentsMogDatabase.filter(
+    (item) => item.id === assignmentsFromClient
+  );
+  return res.json({
+    message: "Complete Fetching assignments",
+    data: assignmentsData[0],
+  });
 });
 
 app.listen(port, () => {
